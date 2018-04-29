f6b4a1d37de972a4d2a00dc30c3dd70f80691a34
danvorobyev <37737526+danvorobyev@users.noreply.github.com>
2018-04-29 16:01:55 +0300

    decode modified

--- decode_inst.c
+++ decode_inst.c
@@ -11,18 +11,43 @@ int exec_command(int16_t* R, char* mem, int16_t* pc){
         ret_status = decode_A_type(inst, R, mem, pc);
         if(ret_status == EXEC_OK)
             break;
+        ret_status = decode_B_type(inst, R, mem, pc);
+        if(ret_status == EXEC_OK)
+            break;
+        ret_status = decode_C_type(inst, R, mem, pc);
+        if(ret_status == EXEC_OK)
+            break;
+        ret_status = decode_D_type(inst, R, mem, pc);
+        if(ret_status == EXEC_OK)
+            break;
+        ret_status = decode_E_type(inst, R, mem, pc);
+        if(ret_status == EXEC_OK)
+            break;
+        ret_status = decode_F_type(inst, R, mem, pc);
+        if(ret_status == EXEC_OK)
+            break;
+        break;
     }
-
 
     return ret_status;
 }
 
+int decode_A_type(int16_t inst, int16_t *R, char *mem, int16_t *pc) // opcode = 16
+{
+    int opcode = slice(inst, 0, 16);
+    switch(opcode)
+    {
+        case HALT:
+            //TODO: halt;
+            return EXEC_OK;
+        default:
+            return EXEC_EXIT;
+    }
+}
 
-int decode_A_type(int16_t inst, int16_t *R, char *mem, int16_t *pc)
+int decode_B_type(int16_t inst, int16_t *R, char *mem, int16_t *pc) // opcode = 4, command = 12
 {
     int opcode = slice(inst, 12, 4);
-    int ss = slice(inst, , );
-    int dd = slice(inst, , );
     switch (opcode)
     {
         case ADD:
@@ -31,8 +56,54 @@ int decode_A_type(int16_t inst, int16_t *R, char *mem, int16_t *pc)
         case MOV:
             //TODO: mov;
             return EXEC_OK;
+        case MOVB:
+            return EXEC_OK;
         default:
             return EXEC_EXIT;
     }
 }
 
+int decode_C_type(int16_t inst, int16_t *R, char *mem, int16_t *pc) // opcode = 7, command = 9
+{
+    int opcode = slice(inst, 9, 7);
+    switch(opcode)
+    {
+        case SOB:
+            //TODO: sob;
+            return EXEC_OK;
+        default:
+            return EXEC_EXIT;
+    }
+}
+
+int decode_D_type(int16_t inst, int16_t *R, char *mem, int16_t *pc) // opcode = 8, command = 8
+{
+    int opcode = slice(inst, 8, 8);
+    switch(opcode)
+    {
+        case BEQ:
+            return EXEC_OK;
+        case BR:
+            return EXEC_OK;
+        default:
+            return EXEC_EXIT;
+    }
+}
+
+int decode_E_type(int16_t inst, int16_t *R, char *mem, int16_t *pc) // opcode = 10, command = 6
+{
+    int opcode = slice(inst, 6, 10);
+    switch(opcode)
+    {
+        case TSTB:
+            //TODO: sob;
+            return EXEC_OK;
+        default:
+            return EXEC_EXIT;
+    }
+}
+
+int decode_F_type(int16_t inst, int16_t *R, char *mem, int16_t *pc) // opcode = 13, command = 3
+{
+
+}
--- decode_inst.h
+++ decode_inst.h
@@ -1,9 +1,15 @@
 #ifndef PDP11_DECODE_INST_H
 #define PDP11_DECODE_INST_H
 
-enum opcode {
-    MOV  = 0x0,
+enum opcode
+{
+    MOV  = 0x1,
     ADD  = 0x6,
+    SOB  = 0x77,
+    MOVB = 0x11,
+    BEQ  = 0x14,
+    BR   = 0x4,
+    TSTB = 0x1057,
     HALT = 0x0
 };
 
@@ -17,5 +23,10 @@ enum exec_command_status
 int exec_command(int16_t* R, char* mem, int16_t* pc);
 
 int decode_A_type(int16_t inst, int16_t *R, char *mem, int16_t *pc);
+int decode_B_type(int16_t inst, int16_t *R, char *mem, int16_t *pc);
+int decode_C_type(int16_t inst, int16_t *R, char *mem, int16_t *pc);
+int decode_D_type(int16_t inst, int16_t *R, char *mem, int16_t *pc);
+int decode_E_type(int16_t inst, int16_t *R, char *mem, int16_t *pc);
+int decode_F_type(int16_t inst, int16_t *R, char *mem, int16_t *pc);
 
 #endif //PDP11_DECODE_INST_H
