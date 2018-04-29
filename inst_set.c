508595641c72be568855c08f667813390f47565e
danvorobyev <manyn2@yandex.ru>
2018-04-29 18:21:47 +0300

    first isa edit

--- inst_set.c
+++ inst_set.c
@@ -1,3 +1,3 @@
 #include "inst_set.h"
 
-extern void  add_op();
+extern void  add_op(int16_t* op1, int16_t* op2, int16_t* pc);
--- inst_set.h
+++ inst_set.h
@@ -6,11 +6,19 @@
 
 //******************** ARITHMETIC OPERATIONS ********************
 
-inline void add_op()
+inline void  add_op(int16_t* op1, int16_t* op2, int16_t* pc)
 {
-
+    int16_t buf;
+    buf = *op1 + *op2;
+    op2 = &buf;
+    *pc += 2;
 }
 
+inline void  mov_op(int16_t* op1, int16_t* op2, int16_t* pc)
+{
+    op2 = op1;
+    *pc += 4;
+}
 
 
 
