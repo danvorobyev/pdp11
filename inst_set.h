500dd4590767889f4c668a22cead8a060d1cabcc
danvorobyev <manyn2@yandex.ru>
2018-05-01 01:38:46 +0300

    all branches added

--- inst_set.h
+++ inst_set.h
@@ -6,16 +6,16 @@
 
 enum bit_scheme // N -> 1_bit; Z -> 2_bit; V -> 3_bit; C -> 4_bit
 {
-    N = 0x1,
-    Z = 0x2,
-    V = 0x4,
-    C = 0x8
+    N_mask = 0x1,
+    Z_mask = 0x2,
+    V_mask = 0x4,
+    C_mask = 0x8
 };
 
 
 //******************** ARITHMETIC OPERATIONS ********************
 
-inline void  add_op(int16_t* op1, int16_t* op2, int16_t* pc)
+inline void  add_op(int16_t* op1, int16_t* op2, int16_t* pc, int16_t* psw)
 {
     *op2 = *op1 + *op2;
     *pc += 2;
@@ -23,13 +23,13 @@ inline void  add_op(int16_t* op1, int16_t* op2, int16_t* pc)
 
 //************************ STORE AND LOAD ************************
 
-inline void  mov_op(int16_t* op1, int16_t* op2, int16_t* pc)
+inline void  mov_op(int16_t* op1, int16_t* op2, int16_t* pc, int16_t* psw)
 {
     *op2 = *op1;
     *pc += 2;
 }
 
-inline void clr_op(int16_t* op1, int16_t* pc)
+inline void clr_op(int16_t* op1, int16_t* pc, int16_t* psw)
 {
     *op1 = 0;
     *pc += 2;
@@ -38,7 +38,7 @@ inline void clr_op(int16_t* op1, int16_t* pc)
 
 //*************************** BRANCH *****************************
 
-inline void sob_op(int16_t* op1, int16_t* op2, int16_t* pc)
+inline void sob_op(int16_t* op1, int16_t* op2, int16_t* pc, int16_t* psw)
 {
     int16_t buf = ((*op2) << 1);
 
@@ -50,88 +50,88 @@ inline void sob_op(int16_t* op1, int16_t* op2, int16_t* pc)
     *pc += 2;
 }
 
-inline void bcc_op(int16_t* offset, int16_t* pc)
+inline void bcc_op(int16_t* offset, int16_t* pc, int16_t* psw) //C = 0
 {
-    *pc += ((psw & C) == 0) ? ((*offset) << 1) : 2; //C = 0
+    *pc += (((*psw & C_mask) >> 3) == 0) ? ((*offset) << 1) : 2;
 }
 
-inline void bcs_op(int16_t * offset, int16_t* pc)
+inline void bcs_op(int16_t * offset, int16_t* pc, int16_t* psw) //C = 1
 {
-    *pc += ((psw & C) == 1) ? ((*offset) << 1) : 2; //C = 1
+    *pc += (((*psw & C_mask) >> 3) == 1) ? ((*offset) << 1) : 2;
 }
 
-inline void beq_op(int16_t * offset, int16_t* pc)
+inline void beq_op(int16_t * offset, int16_t* pc, int16_t* psw) //Z = 1
 {
-
+    *pc += (((*psw & Z_mask) >> 1) == 1) ? ((*offset) << 1) : 2;
 }
 
-inline void bge_op(int16_t * offset, int16_t* pc)
+inline void bge_op(int16_t * offset, int16_t* pc, int16_t* psw) // N ^ V = 0
 {
-
+    *pc += (((*psw & N_mask) ^ ((*psw & V_mask) >> 2)) == 0) ? ((*offset) << 1) : 2;
 }
 
-inline void bgt_op(int16_t * offset, int16_t* pc)
+inline void bgt_op(int16_t * offset, int16_t* pc, int16_t* psw) // Z * (N ^ V) = 0
 {
-
+    *pc += (((*psw & Z_mask) >> 1) & ((*psw & N_mask) ^ ((*psw & V_mask) >> 2)) == 0) ? ((*offset) << 1) : 2;
 }
 
-inline void bhi_op(int16_t * offset, int16_t* pc)
+inline void bhi_op(int16_t * offset, int16_t* pc, int16_t* psw) // C * Z = 0
 {
-
+    *pc += ((((*psw & C_mask) >> 3) ^ ((*psw & Z_mask) >> 1)) == 0) ? ((*offset) << 1) : 2;
 }
 
-inline void bhis_op(int16_t * offset, int16_t* pc)
+inline void bhis_op(int16_t * offset, int16_t* pc, int16_t* psw) // C = 0
 {
-
+    *pc += (((*psw & C_mask) >> 3) == 0) ? ((*offset) << 1) : 2;
 }
 
-inline void ble_op(int16_t * offset, int16_t* pc)
+inline void ble_op(int16_t * offset, int16_t* pc, int16_t* psw) // Z * (N ^ V) = 1
 {
-
+    *pc += (((*psw & Z_mask) >> 1) & ((*psw & N_mask) ^ ((*psw & V_mask) >> 2)) == 1) ? ((*offset) << 1) : 2; // Z * (N ^ V) = 1
 }
 
-inline void blo_op(int16_t * offset, int16_t* pc)
+inline void blo_op(int16_t * offset, int16_t* pc, int16_t* psw) // C = 1
 {
-
+    *pc += (((*psw & C_mask) >> 3) == 1) ? ((*offset) << 1) : 2;
 }
-inline void blos_op(int16_t * offset, int16_t* pc)
+inline void blos_op(int16_t * offset, int16_t* pc, int16_t* psw) // C * Z = 1
 {
-
-}
-
-inline void blt_op(int16_t * offset, int16_t* pc)
-{
-
+    *pc += ((((*psw & C_mask) >> 3) ^ ((*psw & Z_mask) >> 1)) == 1) ? ((*offset) << 1) : 2;
 }
 
-inline void bmi_op(int16_t * offset, int16_t* pc)
+inline void blt_op(int16_t * offset, int16_t* pc, int16_t* psw) // N ^ V = 1
 {
-
+    *pc += (((*psw & N_mask) ^ ((*psw & V_mask) >> 2)) == 1) ? ((*offset) << 1) : 2;
 }
 
-inline void bne_op(int16_t * offset, int16_t* pc)
+inline void bmi_op(int16_t * offset, int16_t* pc, int16_t* psw) // N = 1
 {
-
+    *pc += ((*psw & N_mask) == 1) ? ((*offset) << 1) : 2;
 }
 
-inline void bpl_op(int16_t * offset, int16_t* pc)
+inline void bne_op(int16_t * offset, int16_t* pc, int16_t* psw) // Z = 0
 {
-
+    *pc += (((*psw & Z_mask) >> 1) == 0) ? ((*offset) << 1) : 2;
 }
 
-inline void br_op(int16_t * offset, int16_t* pc)
+inline void bpl_op(int16_t * offset, int16_t* pc, int16_t* psw) // N = 0
 {
-
+    *pc += ((*psw & N_mask) == 0) ? ((*offset) << 1) : 2;
 }
 
-inline void bvc_op(int16_t * offset, int16_t* pc)
+inline void br_op(int16_t * offset, int16_t* pc, int16_t* psw) // Unconditional
 {
-
+    *pc += ((*offset) << 2);
 }
 
-inline void bvs_op(int16_t * offset, int16_t* pc)
+inline void bvc_op(int16_t * offset, int16_t* pc, int16_t* psw) // V = 0
 {
+    *pc += (((*psw & V_mask) >> 2) == 0) ? ((*offset) << 1) : 2;
+}
 
+inline void bvs_op(int16_t * offset, int16_t* pc, int16_t* psw) // V = 1
+{
+    *pc += (((*psw & V_mask) >> 2) == 1) ? ((*offset) << 1) : 2;
 }
 
 
